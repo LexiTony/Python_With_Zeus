f8eead9283ed25a19a6e414d06691c6d233f7a50
Tony <luthorxander.inc@gmail.com>
2022-10-18 14:13:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,325 @@
+# import math
+# print(math.pi)
+
+# width_cm = 5000
+# length_m = 500
+# width_m = width_cm/100
+# perimeter = 2*length_m+2*width_m
+# print(f"Perimeter of field with width{width_cm}cm\and length{length_m}m is{perimeter}m")
+
+# Name=input("What is your name?")
+# print(f"My name is {Name}")
+# Age=input("How old are you?")
+# Age=int(Age)
+# print(f"I am {Age} years old")
+# print(f"In 5 years I will be {Age+5}years old")
+# Weight=input("What is your weight?")
+# print(f"I am {Weight}kgs")
+
+# Radius = input("What is your radius?")
+# Radius = int(Radius)
+
+# Perimeter = 2 * math.pi * Radius
+# print(f"My Perimeter is {Perimeter}")
+
+# Area = math.pi * Radius ** 2
+# print(f"My Area is {Area}")
+
+# Height = input("What is your height?")
+# Height = int(Height)
+# Volume = math.pi * (Radius ** 2) * Height
+# print(f"My volume is {Volume}")
+
+# Income = int(input("What is your monthly income?"))
+# Expense = int(input("What is your monthly expense?"))
+# Percentage_Saved = 100 - (Expense / Income * 100)
+# print(f"My Percentage Saved is {Percentage_Saved})
+
+
+# First_Wife = int(input("How much are you giving your first wife her?"))
+# Second_Wife = int(input("How much are your second wife giving her?"))
+# Child_1 = int(input("How much are your giving first child?"))
+# Child_2 = int(input("How much are your giving second child?"))
+# Child_3 = int(input("How much are your giving third child?"))
+
+
+# Total_Salary = First_Wife+Second_Wife+Child_1+Child_2+Child_3
+
+# Percentage_First_Wife = First_Wife/Total_Salary*100
+# Percentage_Second_Wife = Second_Wife/Total_Salary*100
+# Percentage_Child_1 = Child_1/Total_Salary*100
+# Percentage_Child_2 = Child_2/Total_Salary*100
+# Percentage_Child_3 = Child_3/Total_Salary*100
+
+# print(f"The percentage for First wife is {Percentage_First_Wife}%\n"
+#       f"The percentage for Second wife is {Percentage_Second_Wife}%\n"
+#       f"The percentage for Child_1 is {Percentage_Child_1}%\n"
+#       f"The percentage for Child_2 is {Percentage_Child_2}%\n"
+#       f"The percentage for Child_3 is {Percentage_Child_3}")
+
+# Tax_rate = 0.5
+# Salary_Month = float(input("What is your salary per month?"))
+# Salary_Month = float(Salary_Month)
+# if(Salary_Month>10000):
+#     Net = Salary_Month - (Salary_Month*Tax_rate)
+#     print(f"Your net income is {Net}")
+# else:
+#     print("Your income is too small")
+# print(f"Your net income {Salary_Month}")
+
+
+# Age = int(input("Enter your age."))
+# if Age >= 13:
+# print(f" With the age of {Age} go to the  bigger gas chamber.")
+# else:
+# print(f"With the age of {Age} go to gas chambers")
+
+# INPUT HEIGHT/TALL\SHORT
+# Height = float(input("Enter your height."))
+# if Height >= 140:
+#     print(f"Your height is {Height}cm, and you are tall asf")
+# else:
+#     print(f"Your height is {Height}cm and, you're short as Kevin Hart")
+
+
+# FINDING THE BIGGEST NUMBER GIVEN A PARTICULAR SET OF NUMBERS
+# A = int(input("Enter the value of A"))
+# B = int(input("Enter the value of B"))
+# C = int(input("Enter the value of C"))
+
+
+# if A > B and A > C:
+#     print(f "YOUR VALUE IS {A}\nIT IS YOUR BIGGEST VALUE")
+# if B > A and B > C:
+#     print(f "YOUR VALUE IS {B}\nIT IS YOUR BIGGEST VALUE")
+# else:
+#     print(f "YOUR VALUE IS {C}\nIT IS YOUR BIGGEST VALUE")
+
+
+# Jollof = 856
+# fried_rice = 65
+# chicken = 99
+#
+#
+# Desired_food = input("Please Enter Your Desired Food")
+#
+# if Desired_food == "Jollof and fried_rice and chicken":
+#     print(f"Value is {Jollof+fried_rice+chicken}")
+
+
+# Score = int(input("ENTER YOUR PERCENTAGE."))
+# if Score >= 50:
+#     print(f"Your Percentage is {Score}% and you have PASSED")
+# else:
+#     print(f"Your percentage is {Score}% and you have FAILED")
+
+# Number = int(input("Enter any number."))
+# if Number < 50 and Number % 2 == 0:
+#     print(f"Your Number is {Number} and it is an EVEN number")
+# elif Number < 50 and Number % 2 == 1:
+#     print(f"Your number is {Number} and it is an ODD number")
+# else:
+#     print(f"Your number is {Number} and it is very useless")
+
+
+# Number = int(input("Enter any number."))
+# if Number <= 50:
+#     if Number % 2 == 0:
+#         print(f"{Number} is an EVEN number")
+#     else:
+#         print(f"{Number} is an ODD number")
+# else:
+#     print("Not interested")
+
+#
+
+
+
+# i = 3
+# while i < 1000:
+#     print(i)
+#     i += 3
+# else:
+#     print("Done")
+#
+
+
+# i = 5
+# while i < 100:
+#     print(i)
+#     i += 5
+# else:
+#     print("Done")
+#
+# for number in range(1, 101):
+#     if number % 2 == 0 or number % 3 == 0:
+#         if number % 2 == 0:
+#             print(f"Your number is {number} and it is an Even number")
+#         else:
+#             print(f"Your number is {number} and it is an Odd number")
+
+
+# i = 1
+# while i < 1000:
+#     if i * 3 > 1000:
+#         break
+#     print(i * 3)
+#     i += 1
+# else:
+#     print("Done")
+#
+
+# for i in range(3):
+#     if i == 1:
+#         continue
+#     print(i)
+# else:
+#     print("Done")
+
+# total = 0
+# for number in range(500, 10000, 2):
+#     total += number
+#
+# print(total)
+#
+#
+# sequence = range(500, 10000, 2)
+# print(sum(sequence))
+
+# l=input("Enter length")
+# w=input("Enter width")
+# l=int(l)
+# w=int(w)
+# volume_rec=l*w
+
+# print(f"Volume is {volume_rec}")
+
+# num = int(input("Enter a number"))
+# if num % 2 == 0:
+#     print(f"Number is {num} and it is an even number")
+# else:
+#     print(f"number is {num} and it is an odd number")
+
+
+
+
+# Hacking method Dont use!!!
+
+# Value = input("Enter your number")
+# total = 0
+# for digit in Value:
+#     total += int(digit)
+#
+# print(total)
+
+
+# a = "5" in "8587655" b = "h" not in "Hello" c = "Sefa" + "kor" d = "Andy" * 10 e = "Sefakor"[3] f = "Angela"[0:2] g
+# = "Python"[2:4] h = "Python"[:4] i = "Python"[4:] j = "Python"[:] l = min([1, 2, 3, 4, 5, -2]) m = ord("A") n =
+# max("abcA") o = "hello".index("l") p = "hello".index("l", 2) q = "hello".index("l", 1, 3) r = "Seffffakor".count(
+# "f") s = list(range(1, 21)) t = ["Sefakor", "Andy", "Adamu","Serwaa", "Dasha"] u = t.remove("Dasha") v = len(t) w =
+# ["Kofi", "Abeku", "Amanda"] x = w.append("Adamu") y = w z = w.copy() a_1 = 3 a_2 = 5 print(f"{a}\n {b}\n {c}\n {
+# d}\n {e}\n {f}\n {g}\n {h}\n {i}\n {j}\n {l}\n {m}\n {n}\n {o}\n {p}\n{q}\n{r}\n{s}\n{t}\n{u}\n{v}\n{w}\n{z}\n")
+# print(f"{a_1}\n {a_2}\n")
+
+
+# num = range(1, 21)
+# for i in num:
+#     print(i)
+# name = "anda"
+# name[-1] = "a"
+# print(name)
+# one = id(name)
+# print(one)
+
+
+# for i in range(1, 20):
+#     if i % 3 == 0 or i % 5 == 0:
+#        print(i)
+#     if i % 3 == 0:
+#         print(f"You have multiples of 3 number is {i}")
+#     elif i % 5 == 0:
+#         print(f"You have multiples of 5 and number is {i}")
+
+
+# age = int(input("Enter your age."))
+# if age >= 18:
+#     print("You may proceed to vote. ")
+# else:
+# print(f"Sorry You Are Not Of Age To Vote\nTry In {18-age} year/s time")
+
+# groot = range(15, 101)
+# for i in groot:
+#     if i % 3 == 0:
+#         print(i)
+#     elif i % 5 == 0:
+#         print(i)
+# else:
+#     print("Done")
+
+# x = 10
+# while x > 0:
+#     if x == 5:
+#         continue
+#     if x == 9:
+#         break
+# else:
+#     print("DONE")
+
+# T = type([])
+# print(T)
+
+# R_X = range(21)
+# for i in R_X:
+#     if i % 2 == 0:
+#         print(i)
+#
+# print(len(R_X))
+
+# num = range(1, 20)
+# for i in num:
+#     if i % 2 ==0 or i % 3 == 0:
+#         print(f"{i}Even")
+#     else:
+#         print(f"{i}Odd")
+
+# x = 2
+# Offset = set(x, 'foobar', 123)
+# print(Offset)
+
+# op = hash(45)
+# print(op)
+
+# number = [36, 856, 89, 7436, 8756]
+# x = max(number)
+# n = min(number)
+# print(f"Maximum is {x}\nMinimum is {n}")
+
+#
+# x = 90-10*5
+# Dude = eval('x+50')
+# print(Dude)
+
+
+# pow(base, exp[, mod])
+# jon = pow(38, -1, 97)
+# print(jon)
+# jon2 = 23 * 38 % 97 == 1
+# print(jon2)
+
+
+# percentage = score/total*100
+# per = s/t*100
+# per(s,t[100])
+# per(15, 20)
+
+
+num = float(input("Enter number:"))
+if num > 0:
+    print(f"{num} is positive")
+elif num < 0:
+    print(f"{num} is negative")
+elif num == 0:
+    print(f"You have entered Zero({num})")
+
+gh = 18, "Sept", 13, "Oct"
+gt = 600, "GH"
